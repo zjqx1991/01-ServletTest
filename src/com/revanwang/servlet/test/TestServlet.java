63dca7fc492784738c7f77c9fab4fdb7f6fb4034
RevanWang <zjqx1991@163.com>
2019-06-19 11:15:54 +0800

    Servlet测试类

---
+++ src/com/revanwang/servlet/test/TestServlet.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.revanwang.servlet.test;
+
+import java.io.IOException;
+
+import javax.servlet.Servlet;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+
+/**
+ * @Desc 	
+ * @author Revan Wang
+ *
+ * @Date Jun 19, 201911:12:04 AM
+ */
+public class TestServlet implements Servlet {
+
+	@Override
+	public void init(ServletConfig config) throws ServletException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public ServletConfig getServletConfig() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
+		System.out.print("Servlet初体验");
+		
+	}
+
+	@Override
+	public String getServletInfo() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		
+	}
+	
+}
